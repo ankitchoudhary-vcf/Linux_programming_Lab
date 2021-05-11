33e25efc18108d7bb787e0963ec000de550059dd
root <root@LAPTOP-1GPMOABA.localdomain>
2021-05-11 17:00:07 +0530

    Leap Year Program

---
+++ LeapYear.sh
@@ -0,0 +1,10 @@
+
+
+read -p "Enter the Year y = " y
+
+if [ $(($y % 4)) == 0 ] && [ $(($y % 400)) == 0 ] || [ $(($y % 100)) != 0 ]
+then
+    echo $y" Year is Leap"
+else
+    echo $y" Year is not Leap"
+fi
