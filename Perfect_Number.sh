90c2d35f09f613f0c0840e8717d235aabee4957f
ankit <ac2000choudhary@gmail.com>
2021-05-25 15:02:14 +0530

    Perfect Number

---
+++ Perfect_Number.sh
@@ -0,0 +1,22 @@
+
+
+# Write a shell script to check wether the number is perfect or not
+
+
+read -p "Enter the number >> " n
+
+result=0
+for((i=1; i<n; i++))
+do
+    if [ $((n%i)) -eq 0 ]
+    then
+        result=$((result+i))
+    fi
+done
+
+if [ $result -eq $n ]
+then
+    echo "The Number $n is Perfect"
+else
+    echo "The Number $n is not Perfect"
+fi
