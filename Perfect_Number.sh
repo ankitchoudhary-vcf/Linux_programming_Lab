5c1e59b986a14c586e212dba6e7988a2a4f25e0f
ankit <ac2000choudhary@gmail.com>
2021-06-01 14:55:15 +0530

    Perfect number from 1 to n

--- Perfect_Number.sh
+++ Perfect_Number.sh
@@ -20,3 +20,26 @@ then
 else
     echo "The Number $n is not Perfect"
 fi
+
+
+# find Perfect number between 1 to 100
+
+read -p "Enter the number n >> " n
+
+echo "The Perfect number between 1 and $n :-"
+
+for((j=1; j<=n; j++))
+do
+    result=0
+    for((i=1; i<=j/2; i++))
+    do
+        if [ $((j%i)) -eq 0 ]
+        then
+            result=$((result+i))
+        fi
+    done
+    if [ $result -eq $j ]
+    then
+        echo "$j"
+    fi
+done
