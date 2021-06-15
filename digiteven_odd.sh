32d2c9f22750845d8d20b17a730b635103581e2b
ankit <ac2000choudhary@gmail.com>
2021-06-15 15:41:31 +0530

    Method-3

--- digiteven_odd.sh
+++ digiteven_odd.sh
@@ -57,3 +57,31 @@ done
 echo "Number of even and odd digits in $temp :-"
 echo "Even = $even"
 echo "Odd = $odd"
+
+#Method-3
+
+read -p "Enter the number >> " n
+
+even=0
+odd=0
+n=$(bc<<<$n)
+temp=$n
+while [ $n -gt 0 ]
+do
+
+        if [ $(((n%10)%2)) -eq 1 ]
+        then
+            echo "odd is $((n%10))"
+            odd=$((odd+1))
+        elif [ $(((n%10)%2)) -eq 0 -a $((n%10)) != 0 ]
+        then
+            echo "Even is $((n%10))"
+            even=$((even+1))
+        fi
+
+    n=$((n/10))
+done
+
+echo "Number of even and odd digits in $temp :-"
+echo "Even = $even"
+echo "Odd = $odd"
