dafb2435e444e5c3d179712d10867fea70d18ff8
ankit <ac2000choudhary@gmail.com>
2021-06-22 15:10:57 +0530

    dynamic array

--- dynamic_array.sh
+++ dynamic_array.sh
@@ -1,13 +1,10 @@
 
 read -p "Enter the length of the array: " n
 
-declare arr[$n]
-
 i=0
-while [ $n -gt 0 ]
+while [ $i -lt $n ]
 do
     read -p "Enter the array element >> " arr[$i]
-    n=$(($n-1))
     i=$(($i+1))
 done
 
