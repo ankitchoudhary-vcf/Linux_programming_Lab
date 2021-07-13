7b16fbea070aa3e2e9c1c6643c80a1cadfcb16e8
ankit <ac2000choudhary@gmail.com>
2021-07-13 15:02:24 +0530

    Diamond Parttern

---
+++ Diamond.sh
@@ -0,0 +1,45 @@
+# shell script for pattern printing
+
+read -p "Enter the number of rows : " n
+
+for(( i=1;i<=n;i++))
+do
+    for(( j=i;j<=n-1; j++))
+    do
+        echo -n " "
+    done
+    num=$i
+    for((j=1; j<=2*i-1; j++))
+    do
+        if [ $j -lt $i ]
+        then
+            echo -n "$num"
+            num=$((num+1))
+        else
+            echo -n "$num"
+            num=$((num-1))
+        fi
+    done
+    echo
+done
+
+for(( i=n-1;i>=1;i--))
+do
+    for(( j=i;j<=n-1; j++))
+    do
+        echo -n " "
+    done
+    num=$i
+    for((j=1; j<=2*i-1; j++))
+    do
+        if [ $j -lt $i ]
+        then
+            echo -n "$num"
+            num=$((num+1))
+        else
+            echo -n "$num"
+            num=$((num-1))
+        fi
+    done
+    echo
+done
