22bf00694971175b64105a4cdb6919536b1c1832
ankit <ac2000choudhary@gmail.com>
2021-07-13 14:20:19 +0530

    Right Triangle Pattern

---
+++ rightTrangle.sh
@@ -0,0 +1,12 @@
+# shell script for pattern printing
+
+read -p "Enter the number of rows : " n
+
+for(( i=1;i<=n;i++))
+do
+    for(( j=1;j<=i; j++))
+    do
+        echo -n " * "
+    done
+    echo
+done
